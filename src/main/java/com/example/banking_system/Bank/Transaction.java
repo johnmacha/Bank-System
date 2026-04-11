4a6720a9fcc7b1d12b64d096b0471f08d61fd9c4
johnmacha <jontehmacha@gmail.com>
2026-04-11 17:58:44 +0300

    refactor: made Transaction a real Entity

--- src/main/java/com/example/banking_system/Bank/Transaction.java
+++ src/main/java/com/example/banking_system/Bank/Transaction.java
@@ -1,22 +1,35 @@
 package com.example.banking_system.Bank;
 
-import jakarta.persistence.Embeddable;
+import java.time.LocalDateTime;
 
-@Embeddable
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity
 public class Transaction {
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
-// private Long id;
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 private String type; //Deposit or Withdraw
 private double amount;
+private LocalDateTime timestamp;
 
-// @ManyToOne
-// private BankAccount bankAccount;
+@ManyToOne
+@JoinColumn (name="account_name")
+private BankAccount account;
+
 public Transaction(){}
 
-public Transaction (String type, double amount){
+public Transaction (String type, double amount, BankAccount account){
     this.type = type;
     this.amount = amount;
+    this.timestamp = LocalDateTime.now();
+    this.account = account;
 }
 // Add getters
     public String getType(){
