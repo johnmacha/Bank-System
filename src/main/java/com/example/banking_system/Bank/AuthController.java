f2a1afeefd7b0c887ed543a8cb0c9de771b16cbe
johnmacha <jontehmacha@gmail.com>
2026-04-29 20:16:47 +0300

    refactor: added user authentication

---
+++ src/main/java/com/example/banking_system/Bank/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.banking_system.Bank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user){
+        userRepository.save(user);
+        return "User registered";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        User existing = userRepository.findByUsername(user.getUsername())
+        .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if(!existing.getPassword().equals(user.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+        return jwtUtil.generateToken(user.getUsername());
+    }
+}
---
+++ src/main/java/com/example/banking_system/Bank/User.java
@@ -0,0 +1,39 @@
+package com.example.banking_system.Bank;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String username;
+private String password;
+
+// getters
+public Long getId(){
+    return id;
+}
+public String getUsername(){
+    return username;
+}
+
+public String getPassword(){
+    return password;
+}
+
+// setters
+public void setId(Long id){
+    this.id = id;
+}
+public void setUsername(String username){
+    this.username = username;
+}
+public void setPassword(String password){
+    this.password = password;
+}
+}
